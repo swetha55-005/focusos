0034ba19c165df06df8589bbc2a621f84489a25a
swetha55-055 <srswetha064@gmail.com>
2026-05-22 11:27:32 +0530

    feat: initial project setup

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+
+function App() {
+    return (
+    <div className="min-h-screen bg-black flex items-center justify-center">
+      <h1 className="text-4xl font-bold text-white">
+        Tailwind is working! 🎉
+      </h1>
+    </div>
+  )
+
+}
+
+export default App
